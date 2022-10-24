dfb09a331631496367fdc006e4eb4af239426c7c
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-10-24 15:21:25 -0700

    dded drivers to seed db

--- db/seeds.sql
+++ db/seeds.sql
@@ -24,3 +24,27 @@ VALUES
 ('Haas VF-22','Ferrari',8),
 ('Williams FW44','Mercedes',10),
 ('Ferrari F1-75','Ferrari',3),
+
+
+INSERT INTO driver (first_name, last_name, engine,team_id)
+VALUES
+('Lewis', 'Hamilton',1, NULL),
+('George', 'Russel',1, NULL),
+('Max', 'Verstappen',2, NULL),
+('Sergio', 'Perez',2, NULL),
+('Charles', 'Leclerc',3, NULL),
+('Carlos', 'Sainz',3, NULL),
+('Lando', 'Norris',5, NULL),
+('Daniel', 'Riccaiardo',5, NULL),
+('Esteban', 'Ocon',4, NULL),
+('Fernando', 'Alonso',4, NULL),
+('Valtteri', 'Bottas',6, NULL),
+('Zhou', 'Guanyu',6, NULL),
+('Sebastian', 'Vettel',7, NULL),
+('Lance', 'Stroll',7, NULL),
+('Yuki', 'Tsunoda',9, NULL),
+('Pierre', 'Gasly',9, NULL),
+('Kevin', 'Magnussen',8, NULL),
+('Mick', 'Schumacher',8, NULL),
+('Nicholas', 'Latifi',10, NULL),
+('Alexander', 'Albon',10, NULL),
