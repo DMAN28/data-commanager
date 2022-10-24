a0e4547062383d4ca56e115c7aa0d7a2cba87458
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-10-24 15:00:42 -0700

    dded car to the seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,3 +14,13 @@ VALUES
 
 INSERT INTO car (version, engine,team_id)
 VALUES
+('Mercedes-AMG F1 W13 E','Mercedes',1),
+('Red Bull Racing RB18','Red Bull',2),
+('McLaren MCL36','Mercedes',5),
+('Alpine A522','Renault',4),
+('Alfa Romeo C42','Ferrari',6),
+('Aston Martin AMR22','Mercedes',7),
+('AlphaTauri AT03','Red Bull',9),
+('Haas VF-22','Ferrari',8),
+('Williams FW44','Mercedes',10),
+('Ferrari F1-75','Ferrari',3),
